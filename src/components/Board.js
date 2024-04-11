1588f30f8de7f4ea62e1b0c2845c575b0d478c1e
zakj <1453366421@QQ.COM>
2024-04-11 14:45:49 +0800

    初始化版本

---
+++ src/components/Board.js
@@ -0,0 +1,8 @@
+import boardDefault from '../utils'
+board=boardDefault()
+function ()
+const boardSquare = board.map((rowArray, row) => {
+    return rowArray.map(square, col) => {
+        const
+    }
+})
