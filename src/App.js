ad441d4c6aa99e211a5b87f53f3b98344ac4871d
zakj <1453366421@QQ.COM>
2024-04-11 14:49:03 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import  {BoardDefault ,ArrayDisPlay}  from './components/board'
+
+
 
 function App() {
+  const board =BoardDefault();
   return (
     <div className="App">
       <h1>This is React App.</h1>
+      <h1>18*10 Array</h1>
+      <ArrayDisPlay board={board}/>
     </div>
   )
 }
 
-export default App
+
+export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,28 @@
-import boardDefault from '../utils'
-board=boardDefault()
-function ()
-const boardSquare = board.map((rowArray, row) => {
-    return rowArray.map(square, col) => {
-        const
-    }
-})
+// 构建一个数组的方式，采用18*10、数值都为0的数组
+const BoardDefault = () => {
+  const rows = 18;
+  const cols = 10;
+  const array = Array.from(Array(rows), () => Array(cols).fill(0)); // map的方式构建数组
+  return { array }; // 返回包含数组的对象
+};
+
+// 数组渲染到网页上
+const ArrayDisPlay = (props) => {
+  // 解构赋值
+  const board=props
+  return (
+    <div>
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex}>
+          {row.map((value, colIndex) => (
+            <span key={colIndex} style={{ margin: "ipx" }}>
+              {value}
+            </span>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export { BoardDefault, ArrayDisPlay};
